1ac1e6cfeb9cda2007c57cb31e8191c3dd98ebc8
Tom <1479792108@qq.com>
2018-01-31 15:35:49 +0800

    新增加一个试图

--- StreetCrowd/views.py
+++ StreetCrowd/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    cars_list=[]
+    context={'cars_list':cars_list}
+    return render(request,'StreetCrowd/index.html',context)
