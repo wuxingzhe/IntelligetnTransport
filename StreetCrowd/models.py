3465773394431660b3834374e32571cf292530ea
Tom <1479792108@qq.com>
2018-01-31 16:25:18 +0800

    新建一个数据库模型

--- StreetCrowd/models.py
+++ StreetCrowd/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
+
+class CarStatus(models.Model):
+    car_id=models.IntegerField()
+    longitude=models.FloatField();
+    latitude=models.FloatField()
+    speed=models.IntegerField(default=0)
+    direction=models.IntegerField(default=0)
+    time=models.TimeField()
+
+    def __str__(self):
+        return str(self.car_id)+' '+str(self.longitude)+' '+str(self.latitude)
+
+
