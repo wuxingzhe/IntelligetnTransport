47933b3ec5fb0652fc216a032b240d4dc34d062f
闫坤 <yankun1138283845@foxmail.com>
2018-02-19 23:00:43 +0800

    order before query

--- StreetCrowd/views.py
+++ StreetCrowd/views.py
@@ -10,7 +10,7 @@ from StreetCrowd.utils import handle_upload_file, handleFileThread
 def index(request):
     cars_list=[]
     time_id=[]
-    cars=CarStatus.objects.all().order_by("timeID")
+    cars=CarStatus.objects.all()
     for p in cars:
         tmp=[]
         tmp.append(p.longitude)
